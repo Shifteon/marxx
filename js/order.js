5f1a73041d48ba5516724ef8c1a1a60f5c8f07e1
Shifteon <bpwyatt04@gmail.com>
2021-10-07 14:48:39 -0600

    Order stuff

--- js/order.js
+++ js/order.js
@@ -1,47 +1,59 @@
-function changeImage(type) {
-    let image = document.querySelector("#product-image img");
+var design = "arches";
+var color = "black";
+var step = 1;
 
-    let imageSrc = null;
-
-    switch (type) {
-        case "arches":
-            imageSrc = "images/hoodies/sand-front425.jpg";
-            break;
-        case "tetons":
-            imageSrc = "images/hoodies/teton-black425.jpg";
-        default:
-            break;
-    }
-
-    if (imageSrc != null) {
-        image.setAttribute('src', imageSrc);
-    }
+function setDesign(value) {
+    design = value;
+    changeImage();
 }
 
-function buildDesign() {
-    const designs = ["arches", "dunes", "golden-gate", "old-faithful-blue", "old-faithful", "tetons"];
+function setColor(value) {
+    color = value;
+    changeImage();
+}
+
+function changeImage() {
+    let image = document.querySelector("#product-image img");
+
+    let imageSrc = `images/hoodies/${design}_${color}.jpg`;
+    image.setAttribute('src', imageSrc);
+}
+
+function build(values, type, parent) {
     const main = document.querySelector('#hoodie-main');
     const button = document.querySelector('#product-next');
+    parent.innerHTML = "";
 
-    let parent = document.createElement('section');
-    parent.id = "product-selection";
-
-    for (design of designs) {
+    for (value of values) {
         let content = document.createElement('label');
         content.innerHTML =`
-        <input type="radio" name="test" value="${design}" oninput="changeImage(this.value)" checked>
-        <img src="images/designs/${design}.jpg">`;
+        <input type="radio" name="test" value="${value}" oninput="set${type}(this.value)" checked>
+        <img src="images/designs/${value}.jpg">`;
 
         parent.appendChild(content);
     }
 
-    main.insertBefore(parent, button)
+    main.insertBefore(parent, button);
 }
 
-function buildColor() {
-
-}
+const designs = ["arches", "dunes", "goldengate", "oldfaithfulblue", "oldfaithful", "tetons"];
+const colors = ["black", "white", "maroon", "sand"];
 
 window.addEventListener('load', () => {
-    buildDesign();
+    let parent = document.createElement('section');
+    parent.id = "product-selection";
+
+    build(designs, "Design", parent);
+
+    let button = document.querySelector('#product-next');
+    button.addEventListener("click", () => {
+        switch (step) {
+            case 1:
+                build(colors, "Color", parent);
+                step++;
+                break;
+            default:
+                break;
+        }
+    });
 });
